fbe731ab874cbdd6fd43220ad50d19992ecab2b8
denis rodrigues dos santos <denisrodrigues.ita@gmail.com>
2023-08-09 18:05:00 -0300

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import Header from "@/components/Header";
 import React from "react";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Content from "@/components/Content";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Content />
+      <Footer />
     </>
   );
 };
