5f29ca4f4bf7f38242e9144596a90f437fffae5b
denis rodrigues dos santos <denisrodrigues.ita@gmail.com>
2023-08-11 18:23:12 -0300

    add resp

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,18 +1,34 @@
+"use client";
 import React from "react";
 
 const Content = () => {
+  const [response, setResponse] = React.useState("");
+  const [responseUser, setResponseUser] = React.useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setResponseUser(response);
+    setResponse("");
+  };
+
   return (
     <main>
-      <form action="submit">
+      <form onSubmit={handleSubmit}>
         <textarea
-          name="question"
-          id="a"
           className="textarea"
           rows={5}
           maxLength={255}
           placeholder="Digite aqui sua pergunta"
+          value={response}
+          onChange={(e) => setResponse(e.target.value)}
         />
+        <button className="btn" type="submit">
+          Enviar pergunta!
+        </button>
       </form>
+      <div className="respdiv">
+        <p className="resp">{responseUser}</p>
+      </div>
     </main>
   );
 };
